818545563d4c095686336e0d1a1ee1ed41e614a1
dlevenson44 <daniellevenson4@gmail.com>
2024-03-10 14:12:01 -0400

    add notes/code for list definitions, func stuff

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// Can also pass in variables or strings in below definition
+	cards := []string{newCard(), newCard()}
+	// creates new slice and reassigns variable to that
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(card, i)
+	}
+
+	fmt.Println(cards)
+}
+
+// func newCard() (card string) {
+func newCard() string {
+	return "Five of Diamonds"
 }
